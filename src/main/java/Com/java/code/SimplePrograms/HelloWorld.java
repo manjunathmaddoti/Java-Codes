ebc495c50306a057598c33868d8c47b5186b7677
manjunathmaddoti <manjunathmaddoti@gmail.com>
2025-12-25 12:30:45 +0530

    mm

--- src/main/java/Com/java/code/SimplePrograms/HelloWorld.java
+++ src/main/java/Com/java/code/SimplePrograms/HelloWorld.java
@@ -3,8 +3,7 @@ package Com.java.code.SimplePrograms;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hell World");
-        System.out.println("Hell World");
-        System.out.println("Hell World");
+
     }
 }
 
