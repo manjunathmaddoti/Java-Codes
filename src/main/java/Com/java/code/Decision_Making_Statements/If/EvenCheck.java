64ac421729d181b25f364a31a387fe96cf738d9a
manjunathmaddoti <manjunathmaddoti@gmail.com>
2026-01-07 16:23:00 +0530

    Even

--- src/main/java/Com/java/code/Decision_Making_Statements/If/EvenCheck.java
+++ src/main/java/Com/java/code/Decision_Making_Statements/If/EvenCheck.java
@@ -1,13 +1,14 @@
 package Com.java.code.Decision_Making_Statements.If;
 
 import java.util.Scanner;
-public class EvenCheck {
-    public static void main(String[] args) {
+public class EvenCheck{
+    public static void main(String [] args){
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the Value :");
+        System.out.println("Enter the value:");
         int num=sc.nextInt();
         if(num%2==0){
             System.out.println(num+ " is Even number");
         }
+
     }
 }
