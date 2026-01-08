965d94dcdf233f873c2a7af4e84c58f04104dc90
manjunathmaddoti <manjunathmaddoti@gmail.com>
2026-01-08 11:56:47 +0530

    fact

---
+++ src/main/java/Com/java/code/Loops/For_Loop/Factorial.java
@@ -0,0 +1,28 @@
+package Com.java.code.Loops.For_Loop;
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+
+        System.out.println("Enter the number :");
+        int n=sc.nextInt();
+        int fact=1;
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        System.out.println(fact);
+    }
+}
+
+/*
+
+Output:
+
+Enter the number :
+5
+
+120
+
+ */
